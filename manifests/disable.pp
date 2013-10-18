b2bc1ea83cb3e0aad84218816e6cc70599020aeb
Peter Pouliot <ppouliot@gmail.com>
2013-10-17 22:41:24 -0400

    adding disable

---
+++ manifests/disable.pp
@@ -0,0 +1,21 @@
+# === Class: windows_7zip::disable
+#
+# This class removes the installed components.
+
+class windows_7zip::disable (
+  $package   = $::windows_7zip::params::package,
+) inherits windows_7zip::params {
+
+  package { $package:
+    ensure  => absent,
+  }
+
+  windows_path { $7zip_path:
+    ensure  => absent,
+  }
+
+  file { "${::temp}/${package}.exe":
+    ensure  => absent,
+  }
+
+}
